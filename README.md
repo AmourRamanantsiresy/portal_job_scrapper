d1a2b8788e6660402000f1d087ebbb15b4779f68
kirioka <hei.amour.2@gmail.com>
2022-07-13 15:04:20 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PORTAL JOB SCRAPPER #
+
+This is a express js server that get all Job offers for each page with the number of the page.
---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cors = require("cors")
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+
+app.get("/:id", (req, response) => {
+    try {
+        const id = parseInt(req.params.id)
+        const urls = `https://www.portaljob-madagascar.com/emploi/liste${id === 1 ? "" : "/page/" + id}`;
+        axios.get(urls)
+            .then(res => {
+                const $ = cheerio.load(res.data);
+                const data = [getLastPage($)];
+                $(".item_annonce").each((k, e) => {
+                    const temp = [];
+                    $(e).children().each((k2, e1) => {
+                        if (k2 != 0) {
+                            $(e1).each((k, e2) => {
+                                $(e2).children().each((k, e3) => {
+                                    temp.push($(e3).html().trim().replaceAll("\n", "").replaceAll('"', "'"));
+                                })
+                            })
+                        }
+                    })
+                    data.push(temp);
+                });
+                response.status(200).json(data);
+            });
+    } catch {
+        response.send("Verify you network")
+    }
+})
+
+app.listen(8001, () => {
+    console.log("Server is started")
+})
+
+//get the last page
+function getLastPage($) {
+    const result = $(".pagination>ul>li");
+    return parseInt($($(result[result.length - 1]).children()).html());
+}
